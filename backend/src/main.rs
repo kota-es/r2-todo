fbb18258ef79ab940a132f794979e6bc7bb47592
kota-es <info@k-3110.com>
2023-04-16 00:42:13 +0900

    build backend

---
+++ backend/src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("backend", 8080))?
+    .run()
+    .await
+}
